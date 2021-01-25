b4a30d7e7d68850c7c80e5c53653410330d8bc27
Miguel Gavilán <miangame1@gmail.com>
2021-01-25 17:30:22 +0100

    Change social order

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -20,24 +20,24 @@ const Contact = () => {
       <Description>Software Engineer | Web Developer</Description>
       <Social>
         <a
-          href="https://twitter.com/miguel5gavilan"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaTwitter size={30} />
-        </a>
-        <a
           href="https://www.linkedin.com/in/miguel-%C3%A1ngel-gavil%C3%A1n-merino-111438138/"
           target="_blank"
           rel="noreferrer"
         >
           <FaLinkedin size={30} />
         </a>
+        <a href="https://github.com/Miangame" target="_blank" rel="noreferrer">
+          <FaGithub size={30} />
+        </a>
         <a href="https://dev.to/miangame" target="_blank" rel="noreferrer">
           <FaDev size={30} />
         </a>
-        <a href="https://github.com/Miangame" target="_blank" rel="noreferrer">
-          <FaGithub size={30} />
+        <a
+          href="https://twitter.com/miguel5gavilan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitter size={30} />
         </a>
         <a href="mailto:miangame1@gmail.com" target="_blank" rel="noreferrer">
           <AiOutlineMail size={30} />
