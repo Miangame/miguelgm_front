1570c7f631d1043898197c3b7d2ef11e47546345
Miguel Gavilán <miangame1@gmail.com>
2021-08-03 11:10:04 +0200

    chore: :memo: fix typos

--- src/data/jobs.ts
+++ src/data/jobs.ts
@@ -27,17 +27,17 @@ export const jobs = [
     iconBackground: colors.wealizeSecondary,
     iconShadow: colors.wealizePrimary,
     abilities: [
-      'Creación de interfaces de usuario usando <b>Typescript, Reactjs/Nextjs, Styled components, SWR, SSR</b>.',
-      'Utilización de sistemas de diseño como <b>Ant.design</b> o <b>Carbon design system</b>.',
-      'Realización de pruebas de control de calidad para descubrir errores y optimizar la usabilidad con <b>Cypress y tests unitarios</b>.',
-      'Generación y envío de tokens en blockchain usando <b>Smart Contracts</b>.',
-      'Creación de <b>API REST</b> con <b>Python+Django</b>.',
-      'Implementación de sistemas de pago con <b>Stripe</b> y <b>PayPal</b>.',
-      'Implementación de CI/CD con <b>GithubActions</b> o <b>CircleCI</b>.',
-      'Uso de contenedores con <b>Docker/Docker-compose</b> y despliegue en <b>Heroku</b>.',
-      'Control de versiones con <b>Git</b> y <b>Github</b>.',
-      '<b>Clean code</b>',
-      '<b>Clean architecture</b>'
+      'Creación de interfaces de usuario usando <strong>Typescript, Reactjs/Nextjs, Styled components, SWR, SSR</strong>.',
+      'Utilización de sistemas de diseño como <strong>Ant.design</strong> o <strong>Carbon design system</strong>.',
+      'Realización de pruebas de control de calidad para descubrir errores y optimizar la usabilidad con <strong>Cypress y tests unitarios</strong>.',
+      'Generación y envío de tokens en blockchain usando <strong>Smart Contracts</strong>.',
+      'Creación de <strong>API REST</strong> con <strong>Python+Django</strong>.',
+      'Implementación de sistemas de pago con <strong>Stripe</strong> y <strong>PayPal</strong>.',
+      'Implementación de CI/CD con <strong>GithubActions</strong> o <strong>CircleCI</strong>.',
+      'Uso de contenedores con <strong>Docker/Docker-compose</strong> y despliegue en <strong>Heroku</strong>.',
+      'Control de versiones con <strong>Git</strong> y <strong>Github</strong>.',
+      '<strong>Clean code</strong>',
+      '<strong>Clean architecture</strong>'
     ]
   },
   {
@@ -52,12 +52,12 @@ export const jobs = [
     iconShadow: colors.eurotransPrimary,
     abilities: [
       'Diseño y creación de interfaces para nuevas funcionalidades con <bClean>Symfony + Twig + Bootstrap</bClean>.',
-      'Creación de API Rest con <b>Symfony</b>.',
+      'Creación de API Rest con <strong>Symfony</strong>.',
       'JQuery.',
       'Refactor de código monolítico.',
       'Despliegue a producción en VPS.',
-      'Automatización de tareas del servidor con <b>Bash</b>.',
-      'Creación de consultas complejas con <b>SQL</b>.'
+      'Automatización de tareas del servidor con <strong>Bash</strong>.',
+      'Creación de consultas complejas con <strong>SQL</strong>.'
     ]
   },
   {
@@ -71,8 +71,8 @@ export const jobs = [
     iconBackground: colors.sopinetBackground,
     iconShadow: colors.sopinetPrimary,
     abilities: [
-      'Creación de interfaces para herramientas internas de la empresa con <b>Symfony+Twig</b>.',
-      'Creación de <b>API Rest</b> con <b>Symfony</b>',
+      'Creación de interfaces para herramientas internas de la empresa con <strong>Symfony+Twig</strong>.',
+      'Creación de <strong>API Rest</strong> con <strong>Symfony</strong>',
       'Creación y mantenimiento de un bot de telegram hecho con Symfony.'
     ]
   }
