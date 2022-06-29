6e6f5901c27c0c8446530a816bc24da3f09e7b09
Miguel Gavilán <miangame1@gmail.com>
2022-06-29 09:57:44 +0200

    fix: :fire: image routes

--- src/data/presentations.ts
+++ src/data/presentations.ts
@@ -5,27 +5,27 @@ export const presentations = [
     title: 'sysmana_2020.title',
     description: 'sysmana_2020.description',
     code: 'https://github.com/Miangame/Sysmana2020',
-    image: 'img/next-js.png',
+    image: '/img/next-js.png',
     schedule: 'https://informatica.iesgrancapitan.org/sysmanas/sysmana-xii/'
   },
   {
     title: 'sysmana_2019.title',
     description: 'sysmana_2019.description',
     code: 'https://github.com/Miangame/Sysmana2019',
-    image: 'img/symfony.png',
+    image: '/img/symfony.png',
     schedule: 'https://informatica.iesgrancapitan.org/sysmanas/sysmana-xi/'
   },
   {
     title: 'sysmana_2018.title',
     description: 'sysmana_2018.description',
     code: 'https://github.com/Miangame/sysmana-reactJS',
-    image: 'img/vue-vs-react.jpg',
+    image: '/img/vue-vs-react.jpg',
     schedule: 'https://informatica.iesgrancapitan.org/sysmanas/sysmana-x/'
   },
   {
     title: 'sysmana_2017.title',
     description: 'sysmana_2017.description',
-    image: 'img/raspberry.jpg',
+    image: '/img/raspberry.jpg',
     schedule: 'https://informatica.iesgrancapitan.org/sysmanas/sysmana-ix/'
   }
 ] as Presentation[]
