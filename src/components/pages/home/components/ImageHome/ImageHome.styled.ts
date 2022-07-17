8c6aab69344ff6a5a8e6f9fb41b9cbc725395646
Miguel Gavilán <miangame1@gmail.com>
2022-07-17 14:00:44 +0200

    feat: change image to next/img

--- src/components/pages/home/components/ImageHome/ImageHome.styled.ts
+++ src/components/pages/home/components/ImageHome/ImageHome.styled.ts
@@ -1,18 +1,30 @@
 import styled from 'styled-components'
+import Image from 'next/image'
 
 import { media } from '../../../../../theme/media'
 
 export const ImageHomeContainer = styled.picture`
   width: 100%;
-  text-align: center;
+  display: flex;
+  justify-content: center;
 `
 
-export const ProfileImageHome = styled.img`
+export const ProfileImageHome = styled(Image)`
   border-radius: 50%;
-  width: 130px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+`
+
+export const ProfileImageHomeContainer = styled.div`
+  position: relative;
+  width: 130px;
+  height: 130px;
+
+  & > span {
+    overflow: initial !important;
+  }
 
   ${media.greaterThan('md')`
     width: 170px;
+    height: 170px;
   `}
 `
--- src/components/pages/home/components/ImageHome/ImageHome.tsx
+++ src/components/pages/home/components/ImageHome/ImageHome.tsx
@@ -1,10 +1,22 @@
 import React, { memo } from 'react'
 
-import { ImageHomeContainer, ProfileImageHome } from './ImageHome.styled'
+import {
+  ImageHomeContainer,
+  ProfileImageHome,
+  ProfileImageHomeContainer
+} from './ImageHome.styled'
 
 const ImageHome = (): JSX.Element => (
   <ImageHomeContainer>
-    <ProfileImageHome alt="profile" src="/img/profile.webp" loading="lazy" />
+    <ProfileImageHomeContainer>
+      <ProfileImageHome
+        alt="profile"
+        src="/img/profile.webp"
+        loading="lazy"
+        layout="fill"
+        objectFit="contain"
+      />
+    </ProfileImageHomeContainer>
   </ImageHomeContainer>
 )
 
